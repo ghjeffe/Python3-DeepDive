f39029a5bdcdbedb4c0c15d712550c0598f8f3f6
Gary Harland Jeffers II <garyjeffersii@gmail.com>
2020-08-19 00:53:24 -0500

    swedish

---
+++ part02/project1/CustomSequence.py
@@ -0,0 +1,42 @@
+from functools import lru_cache
+
+
+class Silly():
+    def __init__(self, n):
+        self.n = n
+
+    def __len__(self):
+        print('Called __len__')
+        return self.n
+
+    def __getitem__(self, value):
+        # print(f'You requested item at {value}')
+        if value < 0 or value >= self.n:
+            raise IndexError
+        else:
+            return 'Big, giant buns!'
+
+
+class Fib():
+    def __init__(self, n):
+        self.n = n
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, s):
+        if isinstance(s, int):
+            if s < 0:
+                s = self.n + s
+            if s < 0 or s >= self.n:
+                raise IndexError
+            else:
+                return Fib._fib(s)
+
+    @staticmethod
+    @lru_cache(2 ** 10)
+    def _fib(n):
+        if n < 2:
+            return 1
+        else:
+            return Fib._fib(n-1) + Fib._fib(n-2)
